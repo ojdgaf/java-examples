2b554d17de286054bf091241930b016effdf06e3
Eugene Burdeinyi <ojdgaf@gmail.com>
2019-04-12 19:39:18 +0300

    added json response from index endpoint

--- examples/bootapp/src/main/java/com/ojdgaf/examples/bootapp/config/SecurityConfig.java
+++ examples/bootapp/src/main/java/com/ojdgaf/examples/bootapp/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
-                .antMatchers("/login", "/register").anonymous()
+                .antMatchers("/", "/login", "/register").anonymous()
                 .antMatchers("/api/users/**", "/api/roles/**").hasAuthority("ROLE_ADMIN")
                 .antMatchers("/api/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .anyRequest().authenticated();
--- examples/bootapp/src/main/java/com/ojdgaf/examples/bootapp/controllers/HomeController.java
+++ examples/bootapp/src/main/java/com/ojdgaf/examples/bootapp/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.ojdgaf.examples.bootapp.controllers;
 
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public Resource<String> index() {
+        return new Resource<>("Spring Boot Application Example");
     }
 }
